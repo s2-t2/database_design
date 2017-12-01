31becfb036e7aec19f881fc2b1e56e53d6ffa750
Louis Janse van Rensburg <louisjansevanrensburg@student247-55.eduroam.uu.se>
2017-12-01 16:00:09 +0100

    b

--- db.sql
+++ db.sql
@@ -3,7 +3,9 @@ CREATE TABLE Department(id int NOT NULL PRIMARY KEY,
 	link varchar(255) NOT NULL, 
 	title varchar(255) NOT NULL UNIQUE, 
 	description text NOT NULL, 
-	parentId int);
+	parentId int,
+FOREIGN KEY (parentId) REFERENCES Department(id)
+	);
 
 //add average rating to product
 CREATE TABLE Product(id int NOT NULL PRIMARY KEY, 
@@ -13,11 +15,12 @@ CREATE TABLE Product(id int NOT NULL PRIMARY KEY,
 	stockQuantity int NOT NULL, 
 	price_BasePrice decimal(20,2) NOT NULL, 
 	price_Discount int, 
-	price_Vat int NOT NULL); 
-	#price_WithVat decimal(20,2) NOT NULL, derived att. 
-	#price_WithOutVat decimal(20,2) NOT NULL); derived att. 
-
-CREATE TABLE User_Product()
+	price_Vat int NOT NULL,
+    deptID int,
+    FOREIGN KEY (deptID) REFERENCES Product (id)
+	); 
+	-- #price_WithVat decimal(20,2) NOT NULL, derived att. 
+	-- #price_WithOutVat decimal(20,2) NOT NULL); derived att. 
 
 CREATE TABLE Users(id int NOT NULL PRIMARY KEY, 
 	phoneNumber varchar(16), 
@@ -27,12 +30,28 @@ CREATE TABLE Users(id int NOT NULL PRIMARY KEY,
 	address_street varchar(255) NOT NULL, 
 	fullName_FirstName varchar(255) NOT NULL, 
 	fullName_LastName varchar(255) NOT NULL, 
-	personnr varchar(12), 
+	personNr varchar(12), 
 	checkBox bit NOT NULL, 
 	password varchar(255) NOT NULL);
 
 CREATE TABLE User_Product(uid int NOT NULL, 
 	pid int NOT NULL, 
 	Constraint uid_FK FOREIGN KEY (uid) REFERENCES Users (id), 
-	Constraint pid_FK FOREIGN KEY (pid) REFERENCES Department (id), 
+	Constraint pid_FK FOREIGN KEY (pid) REFERENCES Product (id), 
 	Constraint puid_PK PRIMARY KEY (uid, pid));
+
+CREATE TABLE Keywords(
+keyword_id int NOT NULL PRIMARY KEY,
+name varchar NOT NULL UNIQUE
+pid_FK int NOT NULL,
+FOREIGN KEY (pid_FK) REFERENCES Product (id)
+);
+
+CREATE TABLE Order (
+orderID int NOT NULL PRIMARY KEY,
+orderDate varchar(10) NOT NULL,
+lastChangeDate varchar(10) NOT NULL,
+lastChangeDate varchar(10) NOT NULL,
+paymentReference varchar(10) NOT NULL UNIQUE,
+trackingNumber varchar(10) UNIQUE,
+);
