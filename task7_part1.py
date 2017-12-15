8442b44ed783b2effc67f97e601ef573c0a5dd5e
Louis Janse van Rensburg <louisjansevanrensburg@student251-39.eduroam.uu.se>
2017-12-15 16:32:36 +0100

    commiting python file

--- task7_part1.py
+++ task7_part1.py
@@ -4,24 +4,28 @@
 #Task 7 - part 1
 
 import MySQLdb
+conn = None
+cursor = None
+try:
+    conn = MySQLdb.connect("back.db1.course.it.uu.se", "fall17_it7", "aaaCbaQp", "fall17_project_it7")  # hostname, username, password, database
+    conn.autocommit(True)
+    cursor = conn.cursor()
 
-conn = MySQLdb.connect("back.db1.course.it.uu.se", "fall17_it7", "aaaCbaQp", "fall17_project_it7")  # hostname, username, password, database
-conn.autocommit(True)
-cursor = conn.cursor()
+    while True:
+        department_id = int(raw_input("Enter Department id (0 to exit): "))
+        if department_id == 0:
+            break
 
-while True:
-    department_id = int(raw_input("Enter Department id (0 to exit): "))
-    if department_id == 0:
-        break
-
-    cursor.execute("""
-        SELECT id, title
-        FROM Department
-        WHERE parentId = %s
-    """, (department_id,))  
+        cursor.execute("""
+            SELECT id, title
+            FROM Department
+            WHERE parentId = %s
+        """, (department_id,))  
 
     for row in cursor:
         print "%4d | %32s" % row 
-
-cursor.close()
-conn.close()
+except:
+    print("Please dont be silly")        
+finally:
+    cursor.close()
+    conn.close()
