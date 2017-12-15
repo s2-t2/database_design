4f009dc6898ba047fc26f0b25a6660b037a35ede
sujata5538 <sujata.tamang.5538@student.uu.se>
2017-12-15 17:17:12 +0100

    final

--- task7_part1.py
+++ task7_part1.py
@@ -6,32 +6,41 @@
 import MySQLdb
 conn = None
 cursor = None
+
 try:
     conn = MySQLdb.connect("back.db1.course.it.uu.se", "fall17_it7", "aaaCbaQp", "fall17_project_it7")  # hostname, username, password, database
     conn.autocommit(True)
     cursor = conn.cursor()
 
-
     while True:
         department_id = int(raw_input("Enter Department id (0 to exit): "))
         if department_id == 0:
             break
+        
+        elif department_id <= 11:
+            cursor.execute("""
+                SELECT id, title
+                FROM Department
+                WHERE parentId = %s
+                """, (department_id,))  
+            if cursor.rowcount != 0 :
+                for row in cursor:
+                    print "%4d | %32s" %row
+            else :   
+                cursor.execute("""
+                    SELECT id,title
+                    FROM Product
+                    WHERE Product.deptID = %s
+                    """,(department_id,))
+            
+                if cursor.rowcount != 0 :
+                    for row in cursor:
+                        print "%4d | %32s" %row
 
-        conn = MySQLdb.connect("back.db1.course.it.uu.se", "fall17_it7", "aaaCbaQp", "fall17_project_it7")
-        conn.autocommit(True)
-        cursor = conn.cursor()
-
-
-        cursor.execute("""
-            SELECT id, title
-            FROM Department
-            WHERE parentId = %s
-        """, (department_id,))  
-
-    for row in cursor:
-        print "%4d | %32s" % row 
 except:
-    print("Please dont be silly")        
+    print("Please dont be silly")  
+
 finally:
-    cursor.close()
-    conn.close()
+    if cursor and conn :
+        cursor.close()
+        conn.close()
